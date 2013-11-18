4c8930d3a70f4b4c865fed53e0f670d340e60d0d
Mathias Ritter <mritter@student.tgm.ac.at>
2013-11-18 10:19:06 +0100

    Added RitterSays.java

--- Start.java
+++ Start.java
@@ -2,6 +2,7 @@ public class Start {
 	public static void main(String[] a) {
 		SimonSays[] diskussionsrunde = new SimonSays[28];
 		diskussionsrunde[0] = new ListSays();
+		diskussionsrunde[22] = new RitterSays();
 
 		for (SimonSays s : diskussionsrunde) {
 			if (s != null)  
