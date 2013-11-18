e71bde8654e38a70cf722746a7c516cb00649135
Christoph Hackenberger <chackenberger@student.tgm.ac.at>
2013-11-18 10:12:44 +0100

    Modified Start.java

--- Start.java
+++ Start.java
@@ -2,7 +2,7 @@ public class Start {
 	public static void main(String[] a) {
 		SimonSays[] diskussionsrunde = new SimonSays[28];
 		diskussionsrunde[0] = new ListSays();
-
+		diskussionsrunde[6] = new HackenbergerSays();
 		for (SimonSays s : diskussionsrunde) {
 			if (s != null)  
 				s.say("Let's get started!");
