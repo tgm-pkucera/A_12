76311a8c0fce244965ba3583a54cfaa908126b94
Stefan Erceg <stefan.erceg@student.tgm.ac.at>
2013-11-18 10:19:22 +0100

    added ErcegSays.java

--- Start.java
+++ Start.java
@@ -2,6 +2,7 @@ public class Start {
 	public static void main(String[] a) {
 		SimonSays[] diskussionsrunde = new SimonSays[28];
 		diskussionsrunde[0] = new ListSays();
+		diskussionsrunde[2] = new ErcegSays();
 		diskussionsrunde[12] = new KalaunerSays();
 
 		for (SimonSays s : diskussionsrunde) {
