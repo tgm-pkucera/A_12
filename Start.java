b310543b7166706f12d59444f00d2bdeba235829
Rene8888 <rene.hollander@hotmail.de>
2013-11-18 11:04:27 +0100

    Fixed merge fail

--- Start.java
+++ Start.java
@@ -11,23 +11,11 @@ public class Start {
 		diskussionsrunde[9] = new HollanderSays();
 		diskussionsrunde[10] = new JakubekSays();
 		diskussionsrunde[12] = new KalaunerSays();
-<<<<<<< HEAD
 		diskussionsrunde[20] = new PolydorSays();
-=======
-<<<<<<< HEAD
-		diskussionsrunde[21] = new PücherSays();
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
+		diskussionsrunde[21] = new Pï¿½cherSays();
 		diskussionsrunde[13] = new KalaunerSays();
-=======
-=======
 		diskussionsrunde[24] = new SchwenznerSays();
->>>>>>> 43b6ae4c31b79ca69d230e1df6c4a808f97a93d9
->>>>>>> 042881cabd05961d6f713a8252e04576ac275075
 		diskussionsrunde[26] = new WeinbergerSays();
->>>>>>> c48e6681c4967af73ecacbb915e6db14d00b38ee
->>>>>>> cbd25111f4523210c1c69dca09eae56402aac148
 		diskussionsrunde[27] = new WorthaSays();
 		for (SimonSays s : diskussionsrunde) {
 			if (s != null)  
