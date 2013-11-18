558f788857d4cc2b58ef41d769bf79bfddf8d295
tgm-ttaschner <thomas.taschner1995@hotmail.com>
2013-11-18 10:29:36 +0100

    Meine Änderung

--- Start.java
+++ Start.java
@@ -1,7 +1,8 @@
+package they_say;
 public class Start {
 	public static void main(String[] a) {
 		SimonSays[] diskussionsrunde = new SimonSays[28];
-		diskussionsrunde[0] = new ListSays();
+		diskussionsrunde[25] = new TaschnerSays();
 
 		for (SimonSays s : diskussionsrunde) {
 			if (s != null)  
