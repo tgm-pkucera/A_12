7efda0312eb0b98a01918937e565b8f6c1b0c649
Paul Kalauner <pkalauner@student.tgm.ac.at>
2013-11-21 16:43:03 +0100

    Colored Output support

--- Kalauner/AU05/JGrep.java
+++ Kalauner/AU05/JGrep.java
@@ -1,5 +1,6 @@
 package at.sew.au05;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -8,6 +9,20 @@ import java.util.regex.Pattern;
  *
  */
 public class JGrep {
+	
+	/**
+	 * ANSI Escape Code fuer Rot
+	 */
+	public static final String ANSI_RED = "\u001B[31m";
+	
+	/**
+	 * ANSI Escape Code um die Farbe zurueckzusetzen
+	 */
+	public static final String ANSI_RESET = "\u001B[0m";
+	
+	//Fuer faerbigen Output unter Windows ist eine Library erforderlich
+	
+	
 	/**
 	 * @param input
 	 * @param pattern
@@ -17,8 +32,12 @@ public class JGrep {
 		String output = "";
 		Pattern p = Pattern.compile(pattern);
 		for (String s : input) {
-			if (p.matcher(s).find()) {
-				output += s + "\n";
+			Matcher m = p.matcher(s);
+			if (m.find()) {
+				String norm1 = s.substring(0, m.start());
+				String color = s.substring(m.start(), m.end());
+				String norm2 = s.substring(m.end(), s.length());
+				output += norm1 + ANSI_RED + color + ANSI_RESET + norm2 + "\n";
 			}
 		}
 		return output;
